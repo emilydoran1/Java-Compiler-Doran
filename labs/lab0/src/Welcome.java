2fdd24c7e041b15c233d3834754891847ec82e9c
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2021-02-16 11:12:06 -0500

    adding welcome file to test setup env

---
+++ labs/lab0/src/Welcome.java
@@ -0,0 +1,10 @@
+/**
+ * This Welcome program displays a welcome message and is a test file for the setup environment.
+ *
+ */
+
+public class Welcome {
+    public static void main (String[] args) {
+        System.out.println("Welcome to my compilers repository!");
+    }
+}
