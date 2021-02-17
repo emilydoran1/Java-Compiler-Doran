64b4a91078ba8c9dcbfd217eabb409dbb36bf84b
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2021-02-17 13:52:43 -0500

    allowing compiler.java to take input as command line param and adding test file

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * This program will be the entry point for the compiler
  * @author Emily Doran
@@ -6,7 +11,24 @@
 
 public class Compiler {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception {
         System.out.println("Welcome to my compilers repository!");
+
+        // check if a paramater is entered to read the file contents
+        if(args.length > 0){
+            File file = new File(args[0]);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            try{
+                while (reader.ready()) {
+                    System.out.println(reader.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
 }
