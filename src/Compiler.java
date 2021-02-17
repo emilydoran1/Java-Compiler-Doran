52227560ca1666c4e4a74a54668d68a2c6f9e256
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2021-02-17 13:11:05 -0500

    updating javadoc

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,10 +1,11 @@
 /**
- * This Welcome program displays a welcome message and is a test file for the setup environment.
+ * This program will be the entry point for the compiler
+ * @author Emily Doran
  *
  */
 
 public class Compiler {
-    
+
     public static void main (String[] args) {
         System.out.println("Welcome to my compilers repository!");
     }
