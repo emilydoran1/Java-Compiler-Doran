77694a7b1bf41a9755a9eaf1efdd52db663d9153
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2021-03-01 12:14:16 -0500

    defining basic rule order methods for lexer

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,4 +1,39 @@
 import java.util.regex.Pattern;
 
 public class Lexer {
+    private int lastPosition;
+    private int currentPosition;
+
+    // TODO: define Rule Order
+    // keyword (1)
+    private boolean checkKeyword(String stringToCheck){
+        boolean isKeyword = false;
+        return isKeyword;
+    }
+
+    // id (2)
+    private boolean checkId(String stringToCheck){
+        boolean isId = false;
+        return isId;
+    }
+
+    // symbol (3)
+    private boolean checkSymbol(char charToCheck){
+        boolean isSymbol = false;
+        return isSymbol;
+    }
+
+    // digit (4)
+    private boolean checkDigit(String stringToCheck){
+        String regexInteger = "[0-9]+";
+        boolean isDigit = false;
+        return isDigit;
+    }
+
+    // char (5)
+    private boolean checkChar(char charToCheck){
+        String regexCharacter = "[a-z]";
+        boolean isChar = false;
+        return isChar;
+    }
 }
