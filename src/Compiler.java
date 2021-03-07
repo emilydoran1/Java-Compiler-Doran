a1407fe0c7e9053f5e49ce4ca2c170432bb2e222
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2021-03-06 20:08:00 -0500

    handle error if compiler.java is not run with exactly one parameter

--- src/Compiler.java
+++ src/Compiler.java
@@ -13,10 +13,10 @@ public class Compiler {
     public static void main (String[] args) throws Exception {
 
         // check if a paramater is entered to read the file contents
-        if(args.length > 0){
+        if(args.length == 1){
 
             // prompt user if they would like to run in verbose test mode
-            System.out.println("Would you like to run in verbose test mode? (Y/N)");
+            System.out.println("Would You Like to Run the Lexer in Verbose Test Mode? (Y/N)");
             Scanner scan = new Scanner(System.in);
 
             String verboseTestMode = scan.nextLine();
@@ -27,6 +27,10 @@ public class Compiler {
                 verbose = true;
 
             Lexer lex = new Lexer(args[0], verbose);
+        }
+        else{
+            System.out.println("To Run the Lexer, Enter the File as a Command Line Parameter\n " +
+                    "i.e. \"java Compiler testCases.txt\"");
         }
 
     }
