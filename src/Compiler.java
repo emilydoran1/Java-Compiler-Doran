a28a7758bf5600a28b1c23b4eaaef1a5db75ec89
Alan Labouseur <Alan.Labouseur@Marist.edu>
2021-04-12 14:30:22 -0400

    Implemented my own suggestions.
    It saves us time and soothes my OCD while I'm grading your project.

--- src/Compiler.java
+++ src/Compiler.java
@@ -14,24 +14,10 @@ public class Compiler {
 
         // check if a paramater is entered to read the file contents
         if(args.length == 1){
-
-            // prompt user if they would like to run in verbose test mode
-            System.out.println("Would You Like to Run the Lexer in Verbose Test Mode? (Y/N)");
-            Scanner scan = new Scanner(System.in);
-
-            String verboseTestMode = scan.nextLine();
-
-            boolean verbose = false;
-
-            if(verboseTestMode.equalsIgnoreCase("Y"))
-                verbose = true;
-
-            Lexer lex = new Lexer(args[0], verbose);
-        }
-        else{
-            System.out.println("To Run the Lexer, Enter the File as a Command Line Parameter\n " +
+            Lexer lex = new Lexer(args[0], true);
+        } else{
+            System.out.println("To Run the compiler, Enter the File as a Command Line Parameter\n " +
                     "i.e. \"java Compiler testCases.txt\"");
         }
-
     }
 }
